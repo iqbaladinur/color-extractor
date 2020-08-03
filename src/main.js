d49f4e3108c2ee324e12ff01e5d6a915967f6f3c
iqbal adi nurmansyah <iqbaladinur@gmail.com>
2020-08-04 05:50:52 +0700

    add analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import '@/assets/css/tailwind.css';
+
+if (process.env.NODE_ENV) {
+  Vue.use(VueGtag, {
+    config: { id: 'UA-169647352-4' },
+  });
+}
 
 Vue.config.productionTip = false;
 new Vue({
