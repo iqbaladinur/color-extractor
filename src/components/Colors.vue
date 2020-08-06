c020b08e0baf78cb09e43ef7e38b2bfdbd0c32b0
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2020-08-06 16:20:15 +0700

    changed palete style

--- src/components/Colors.vue
+++ src/components/Colors.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="">
     <div v-if="getTopColors.length < 1" class="flex flex-wrap justify-center">
-      <div class="lg:w-16 w-10 bg-gray-600 lg:h-16 h-10 m-2 rounded-full shadow" :class="{ 'animate' : getExtractionStatus}"></div>
-      <div class="lg:w-16 w-10 bg-gray-400 lg:h-16 h-10 m-2 rounded-full shadow" :class="{ 'animate' : getExtractionStatus}"></div>
-      <div class="lg:w-16 w-10 bg-gray-500 lg:h-16 h-10 m-2 rounded-full shadow" :class="{ 'animate' : getExtractionStatus}"></div>
-      <div class="lg:w-16 w-10 bg-gray-300 lg:h-16 h-10 m-2 rounded-full shadow" :class="{ 'animate' : getExtractionStatus}"></div>
-      <div class="lg:w-16 w-10 bg-gray-200 lg:h-16 h-10 m-2 rounded-full shadow" :class="{ 'animate' : getExtractionStatus}"></div>
+      <div class="lg:w-16 w-10 bg-gray-600 lg:h-16 h-10 m-2 rounded-lg shadow-lg" :class="{ 'animate' : getExtractionStatus}"></div>
+      <div class="lg:w-16 w-10 bg-gray-400 lg:h-16 h-10 m-2 rounded-lg shadow-lg" :class="{ 'animate' : getExtractionStatus}"></div>
+      <div class="lg:w-16 w-10 bg-gray-500 lg:h-16 h-10 m-2 rounded-lg shadow-lg" :class="{ 'animate' : getExtractionStatus}"></div>
+      <div class="lg:w-16 w-10 bg-gray-300 lg:h-16 h-10 m-2 rounded-lg shadow-lg" :class="{ 'animate' : getExtractionStatus}"></div>
+      <div class="lg:w-16 w-10 bg-gray-200 lg:h-16 h-10 m-2 rounded-lg shadow-lg" :class="{ 'animate' : getExtractionStatus}"></div>
     </div>
     <div v-else class="flex flex-wrap justify-center">
       <div
         v-for="(colors, key) in getTopColors"
         :key="key"
-        class="lg:w-16 w-10 lg:h-16 h-10 flex m-2 rounded-full shadow relative"
-        :style="`background-color: ${colors.colorHex};`"
+        class="lg:w-16 w-10 lg:h-16 h-10 flex m-2 rounded-lg relative"
+        :style="`background-color: ${colors.colorHex}; box-shadow: 0 10px 15px -3px ${colors.colorHex.slice(0, 7)}b4, 0 4px 6px -2px ${colors.colorHex.slice(0, 7)}b4;`"
         @click="copyToClipboard(colors.colorHex)"
       >
         <copied :text="contrastingText(colors.colorHex)" :bg="colors.colorHex" class="cursor-pointer" />
