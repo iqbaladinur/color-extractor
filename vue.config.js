2050f36b4d9922dbbbeac75fa7371295b8f4b13c
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2020-08-06 19:34:33 +0700

    testing manifest

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: 'Biancoir',
+    themeColor: '#EBF4FF',
+    msTileColor: '#4A5568',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#EBF4FF',
+  },
+};
