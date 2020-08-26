4eacf6b9c633ceb65bdff6abe97e45b4bde4730d
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2020-08-26 17:04:05 +0700

    fix padding

--- src/components/palettes.vue
+++ src/components/palettes.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="bounce" mode="in-out" appear>
-    <div class="flex justify-center my-6 bg-gray-100 p-2 rounded-xl">
+    <div class="flex justify-center my-6 bg-gray-100 p-1 rounded-xl">
       <Palette
         v-for="(color, key) in colorsHex"
         :key="key"
