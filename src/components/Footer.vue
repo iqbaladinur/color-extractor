2972e288d5124cb5e1e9f65ce1d0351c98b34c49
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2020-09-14 10:20:22 +0700

    added footer information

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,8 @@
         <li>
           Made with ❤️ by <a href="https://github.com/iqbaladinur" class="footer-link">Iqbal Adi</a> |
           <a href="https://www.privacypolicygenerator.info/live.php?token=ipuHipcuvTXh4fCJtSXLkMPZrRJsT6ib" class="footer-link">Privacy Police</a> |
-          <a href="https://www.termsofusegenerator.net/live.php?token=AnPvYI8rmFEQX8uG1lWqANrqpIzOw3hp" class="footer-link">Terms of Use</a>
+          <a href="https://www.termsofusegenerator.net/live.php?token=AnPvYI8rmFEQX8uG1lWqANrqpIzOw3hp" class="footer-link">Terms of Use</a> |
+          Images sources from <a href="https://unsplash.com/" class="footer-link">unsplash.com</a>
         </li>
       </ul>
     </div>
